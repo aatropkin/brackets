1e98ebc088ca150f13da959bb60fe27eebedae66
aatropkin <40324229+aatropkin@users.noreply.github.com>
2022-07-14 09:51:15 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  while (str.length > 0){
+    length = str.length;
+    for (let pair of bracketsConfig){
+      str = str.replace(pair[0] + pair[1],"");
+    }
+    if (length === str.length){
+      return false;
+    }
+  }
+  return true;
 }
